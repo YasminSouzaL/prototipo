5566fb49d3c464416938e4ad7eda718d5651b0f9
Yasmin <souzasmimi@gmail.com>
2023-11-10 10:51:33 -0300

    test

--- utis.py
+++ utis.py
@@ -60,7 +60,7 @@ def rectContour(contours):
         area = cv2.contourArea(i)
         if area > 50:
             peri = cv2.arcLength(i, True)
-            approx = cv2.approxPolyDP(i, 0.02*peri, True)
+            approx = cv2.approxPolyDP(i, 0.02*peri, True) 
             if len(approx) == 4:
                 rectCon.append(i)
     rectCon = sorted(rectCon, key=cv2.contourArea, reverse=True)
@@ -70,3 +70,56 @@ def getCornerPoints(cont):
     peri = cv2.arcLength(cont, True)
     approx = cv2.approxPolyDP(cont, 0.02*peri, True)
     return approx
+
+def reorder(myPoints):
+    myPoints = myPoints.reshape((4, 2))
+    myPointsNew = np.zeros((4, 1, 2), np.int32)
+    add = myPoints.sum(1)
+    # print("add", add)
+    myPointsNew[0] = myPoints[np.argmin(add)]
+    myPointsNew[3] = myPoints[np.argmax(add)]
+    diff = np.diff(myPoints, axis=1)
+    # print("diff", diff)
+    myPointsNew[1] = myPoints[np.argmin(diff)]
+    myPointsNew[2] = myPoints[np.argmax(diff)]
+    # print("NewPoints", myPointsNew)
+    return myPointsNew
+
+
+def splitBoxes(img):
+    rows = np.vsplit(img, 5)
+    boxes = []
+    for r in rows:
+        cols = np.hsplit(r, 5)
+        for box in cols:
+            boxes.append(box)
+    return boxes
+
+def drawGrid(img, questions=5, choices=5):
+    secW = int(img.shape[1]/questions)
+    secH = int(img.shape[0]/choices)
+    for i in range(0, 5):
+        pt1 = (0, secH*i)
+        pt2 = (img.shape[1], secH*i)
+        pt3 = (secW*i, 0)
+        pt4 = (secW*i, img.shape[0])
+        cv2.line(img, pt1, pt2, (255, 0, 0), 2)
+        cv2.line(img, pt3, pt4, (255, 0, 0), 2)
+    return img
+
+def showAnswers(img, myIndex, grading, ans, questions=5, choices=5):
+    secW = int(img.shape[1]/questions)
+    secH = int(img.shape[0]/choices)
+    for x in range(0, questions):
+        myAns = myIndex[x]
+        cX = (myAns*secW)+secW//2
+        cY = (x*secH)+secH//2
+        if grading[x] == 1:
+            myColor = (0, 255, 0)
+        else:
+            myColor = (0, 0, 255)
+            correctAns = ans[x]
+            cv2.circle(img, (correctAns*secW+secW//2,
+                             (x*secH)+secH//2), 30, (0, 255, 0), cv2.FILLED)
+        cv2.circle(img, (cX, cY), 50, myColor, cv2.FILLED)
+    return img
